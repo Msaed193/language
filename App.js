dbb16b82394a3071dbab3361a272f805c20636c0
Msaed193 <msaed.aljebur@gmail.com>
2024-04-09 15:53:40 +0300

    Language app i18next

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {  Text, TouchableOpacity, SafeAreaView, Modal, View, FlatList} from 'react-native';
+import { styles } from './styles/style';
+import { useTranslation } from 'react-i18next';
+import i18next, { languageResources } from './services/i18next';
+import { useState } from 'react';
+import languagesList from './services/languageList.json';
+
 
 export default function App() {
+  const [visible, setVisible] = useState(false);
+  const {t} = useTranslation();
+
+  const changeLng = lng => {
+    i18next.changeLanguage(lng);
+    setVisible(false);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Modal visible={visible} onRequestClose={() => setVisible(false)}>
+        <View style={styles.languagesList}>
+          <FlatList
+            data={Object.keys(languageResources)}
+            renderItem={({item}) => (
+              <TouchableOpacity
+                style={styles.languageButton}
+                onPress={() => changeLng(item)}>
+                <Text style={styles.lngName}>
+                  {languagesList[item].nativeName}
+                </Text>
+              </TouchableOpacity>
+            )}
+          />
+        </View>
+      </Modal>
+      <Text style={styles.text}>{t('welcome')}</Text>
+      <TouchableOpacity style={styles.button} onPress={() => setVisible(true)}>
+        <Text style={styles.buttonText}>{t('change-language')}</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
